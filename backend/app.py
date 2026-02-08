b5c31546267a996dffe0276ea2b82c937e9369a5
SanzharInvincible <sanjarbek160410@gmail.com>
2026-02-08 23:13:37 +0500

    New structure

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, render_template
 import os
+import threading
+import webview
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../frontend/templates", static_folder="../frontend/static")
 
 tasks = []
+
+@app.route("/")
+def index():
+    return render_template("interface.html")
 
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
@@ -19,22 +25,14 @@ def add_task():
         "done": False
     })
     return jsonify(success=True), 201
-
-# новый маршрут для фронтенда
-@app.route("/")
-def serve_frontend():
-    print("Frontend запрошен!")  # добавили лог
-    frontend_dir = os.path.join(os.path.dirname(__file__), "..", "frontend")
-    return send_from_directory(frontend_dir, "Interface.html")
-
-@app.route("/<path:path>")
-def serve_static(path):
-    # Serve files from the frontend folder using an absolute path
-    frontend_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "frontend"))
-    print(f"Static file requested: {path} -> {frontend_dir}")
-    return send_from_directory(frontend_dir, path)
+def start_flask():
+    app.run(debug=False, use_reloader=False)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    t = threading.Thread(target=start_flask)
+    t.daemon = True
+    t.start()
+    webview.create_window("To_Do app", "http://Localhost:5000", width=375, height=700)
+    webview.start()
 
 
